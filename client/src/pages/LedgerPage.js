b4e6ebfc1f2d68c86121675e3881568bf041fc72
Karina Hsu <karina.hsu2000@gmail.com>
2026-02-21 15:43:05 -0500

    Create LedgerPage component for ledger detail view

---
+++ client/src/pages/LedgerPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const LedgerPage = () => {
+    return (
+        <div>
+            <h1>Ledger Detail View</h1>
+            <p>This is where you will see the ledger details.</p>
+            {/* Additional ledger details can be added here */}
+        </div>
+    );
+};
+
+export default LedgerPage;
